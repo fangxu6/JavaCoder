8f4d128ef719c5c07c5696a9aa25744bb7304897
fangxu <fangxu6@gmail.com>
2022-09-20 00:19:37 +0800

    单元测试

---
+++ src/main/java/SubStringTest.java
@@ -0,0 +1,7 @@
+public class SubStringTest {
+    public static void main(String[] args) {
+        String str="我不满20";
+        System.out.println("old:"+str);
+        System.out.println("new:"+str.substring(1,20));
+    }
+}
