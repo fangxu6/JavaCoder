e726f396afa3446a815d8b986261631f2be5de32
fangxu <fangxu6@gmail.com>
2022-10-11 20:13:33 +0800

    thread lock 活锁和解决方法

---
+++ src/main/java/thread/lock/live/Account.java
@@ -0,0 +1,35 @@
+package thread.lock.live;
+
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Account {
+    private int balance;
+    private final Lock lock
+            = new ReentrantLock();
+    // 转账
+    void transfer(Account tar, int amt){
+        while (true) {
+            if(this.lock.tryLock()) {
+                try {
+                    if (tar.lock.tryLock()) {
+                        try {
+                            this.balance -= amt;
+                            tar.balance += amt;
+                        } finally {
+                            tar.lock.unlock();
+                        }
+                    }//if
+                } finally {
+                    this.lock.unlock();
+                }
+            }//if
+        }//while
+    }//transfer
+    //上述代码会出现活锁情况
+    //a对b转账，同时b也对a转账。此时a无法获取b的锁，释放；b无法获取a的锁，释放。如此循环
+
+//    用非阻塞的方式去获取锁，破坏了第五章所说的产生死锁的四个条件之一的“不可抢占”。所以不会产生死锁。但是出现了活锁，如上面的分析
+//    用锁的最佳实践，第三个“永远不在调用其他对象的方法时加锁”，我理解其实是在工程规范上避免可能出现的锁相关问题。
+}
---
+++ src/main/java/thread/lock/live/optimization/Account.java
@@ -0,0 +1,43 @@
+package thread.lock.live.optimization;
+
+import org.apache.commons.lang.math.RandomUtils;
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+class Account {
+
+    private int balance;
+
+    private final Lock lock = new ReentrantLock();
+
+    // 转账
+
+    void transfer(Account tar, int amt) throws InterruptedException {
+
+        boolean flag = true;
+        //RandomUtils.nextLong();
+        while (flag) {
+            if (this.lock.tryLock(RandomUtils.nextLong(), NANOSECONDS)) {
+                try {
+                    if (tar.lock.tryLock(RandomUtils.nextLong(), NANOSECONDS)) {
+                        try {
+                            this.balance -= amt;
+                            tar.balance += amt;
+                            flag = false;
+                        } finally {
+                            tar.lock.unlock();
+                        }
+                    }//if
+                } finally {
+                    this.lock.unlock();
+                }
+            }//if
+        }//while
+    }//transfer
+    //TODO 优化建议如果阻塞在tar.lock.tryLock上一段时间，this.lock是不能释放的。
+}
+
