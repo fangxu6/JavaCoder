d9abbc6c0529abfd9043d012034c070b19df8d7f
fangxu <fangxu6@gmail.com>
2022-10-21 10:48:49 +0800

    CountDownLatch CyclicBarrier 多线程计数器

---
+++ src/main/java/thread/countdownlatchandcyclicbarrier/CheckOriginal.java
@@ -0,0 +1,25 @@
+package main.java.thread.countdownlatchandcyclicbarrier;
+
+/**
+ * className:CheckOriginal
+ * package:main.java.thread.countdownlatchandcyclicbarrier
+ * Description:
+ *
+ * @Date:2022/10/2018:02
+ * @Author:fangxu6@gmail.com
+ */
+public class CheckOriginal {
+    public void Check(){
+
+//        while(存在未对账订单){
+//            // 查询未对账订单
+//            pos = getPOrders();
+//            // 查询派送单
+//            dos = getDOrders();
+//            // 执行对账操作
+//            diff = check(pos, dos);
+//            // 差异写入差异库
+//            save(diff);
+//        }
+    }
+}
---
+++ src/main/java/thread/countdownlatchandcyclicbarrier/countdownlatch/CountDownLatchDemo.java
@@ -0,0 +1,39 @@
+package main.java.thread.countdownlatchandcyclicbarrier.countdownlatch;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * className:CountDownLatchDemo
+ * package:main.java.thread.countdownlatchandcyclicbarrier.countdownlatch
+ * Description:
+ *
+ * @Date:2022/10/2018:06
+ * @Author:fangxu6@gmail.com
+ * TODO imp like parttern
+ */
+public class CountDownLatchDemo {
+    public void Check() {
+
+//        // 创建2个线程的线程池
+//        Executor executor =
+//                Executors.newFixedThreadPool(2);
+//        while (存在未对账订单) {
+//            // 查询未对账订单
+//            executor.execute(() -> {
+//                pos = getPOrders();
+//            });
+//            // 查询派送单
+//            executor.execute(() -> {
+//                dos = getDOrders();
+//            });
+//
+//            /* ？？如何实现等待？？*/
+//
+//            // 执行对账操作
+//            diff = check(pos, dos);
+//            // 差异写入差异库
+//            save(diff);
+//        }
+    }
+}
