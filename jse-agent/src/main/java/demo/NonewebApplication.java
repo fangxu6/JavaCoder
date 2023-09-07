d9de8caacb57479e169f619d9c7db8978dbbf292
fangxu <fangxu6@gmail.com>
2023-09-07 23:02:54 +0800

    springboot commandline

---
+++ jse-agent/src/main/java/demo/NonewebApplication.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * className: NonewebApplication
+ * package: demo
+ * Description:
+ *
+ * @author fangxu6@gmail.com
+ * @since 2023/9/7 22:57
+ */
+@SpringBootApplication
+public class NonewebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NonewebApplication.class, args);
+    }
+}
+
